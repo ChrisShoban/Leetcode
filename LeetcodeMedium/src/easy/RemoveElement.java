9f035f0b83c017a2fc1c7aa1fa2df3e9a2820823
Chris Shoban <Chris.Shoban@gmail.com>
2015-05-30 22:46:33 -0700

    Remove Element

---
+++ LeetcodeMedium/src/easy/RemoveElement.java
@@ -0,0 +1,32 @@
+package easy;
+
+public class RemoveElement {
+
+    public int removeElement(int[] nums, int val) {
+        int back = nums.length - 1;
+        for(int i = nums.length - 1; i >= 0; i--) {
+            if(nums[i] == val) {
+                swap(nums, i, back--);
+            }
+        }
+        return back + 1;
+    }
+    
+    private void swap(int[] nums, int i, int total) {
+        int tmp = nums[i];
+        nums[i] = nums[total];
+        nums[total] = tmp;
+    }
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        RemoveElement test = new RemoveElement();
+        System.out.println(test.removeElement(new int[]{2}, 3));
+        System.out.println(test.removeElement(new int[]{3,3}, 3));
+        System.out.println(test.removeElement(new int[]{4,5}, 4));
+
+    }
+
+}
